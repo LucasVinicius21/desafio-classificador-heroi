4185c5a00692267d508e34ec135a55171311d62a
LucasVinicius21 <naves.vieira@gmail.com>
2023-12-04 12:10:12 -0300

    meu commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Variavel
+let nome="Lucas"
+console.log(nome)
+
+// operadores
+let xp = 200000
+console.log(xp)
+
+
+//Laço de repetições
+
+if(xp<=1000){
+    console.log("O Herói " +(nome), "está no nível Ferro")
+}else if(xp >=1001 && xp <=2000){
+    console.log("O Herói " +(nome), "está no nível Bronze")
+}else if(xp >=2001 && xp <=5000){
+    console.log("O Herói " +(nome), "está no nível Prata")
+}else if(xp >=6001 && xp <=7000){
+    console.log("O Herói " +(nome), "está no nível OURO")
+}else if(xp >=7001 && xp <=8000){
+    console.log("O Herói " +(nome), "está no nível Platina")
+}else if(xp >=8001 && xp <= 9000){
+    console.log("O Herói " +(nome), "está no nível Ascendente")
+}else if(xp >=9001 && xp <= 10000){
+    console.log("O Herói " +(nome), "está no nível Imortal")
+}else{
+    console.log("O Herói " +(nome), "está no nível Radiante")
+}
+
